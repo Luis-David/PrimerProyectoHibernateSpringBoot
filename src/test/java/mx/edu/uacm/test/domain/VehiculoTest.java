cc8ffab1a6854b362f1bb1302031bf8872a14d29
laboratorio <laboratorio@laboratorio-ThinkCentre-M73>
2018-10-01 12:57:51 -0500

    Se agrego la implementacion del metodo testGuardarVehiculo()

--- src/test/java/mx/edu/uacm/test/domain/VehiculoTest.java
+++ src/test/java/mx/edu/uacm/test/domain/VehiculoTest.java
@@ -11,6 +11,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import mx.edu.uacm.Application;
+import mx.edu.uacm.domain.Vehiculo;
 import mx.edu.uacm.repository.VehiculoRepository;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -20,12 +21,21 @@ public class VehiculoTest {
 	public static final Logger log = 
 			LogManager.getLogger(VehiculoTest.class);
 	
-	@Autowired
+	@Autowired//Inyeccion de dependencias
 	VehiculoRepository vehiculoRepository;
 	
 	@Test
 	public void testGuardarVehiculo() {
 		log.debug("Entrando a testGuardarVehiculo");
+		//Se crea el vehiculo
+		Vehiculo v=new Vehiculo();
+		//El id es auto generado por la bd
+		//Agregando el modelo
+		v.setModelo("2018");
+		
+		//Se aguarda el objeto vehiculo en BD
+		vehiculoRepository.save(v);
+		
 	}
 	
 	
